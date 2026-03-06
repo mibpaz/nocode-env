b8a82033eb2ca16f5221fafd752d0c6f8b7a4013
Isadora Paz <mibp@ic.ufal.br>
2026-03-06 11:25:16 -0300

    fix: import icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideIcons } from '@ng-icons/core';
-import { featherCopy, featherDownload, featherEdit, featherExternalLink, featherMinus, featherMonitor, featherPlus, featherTrash, featherX } from '@ng-icons/feather-icons';
+import { featherCopy, featherDownload, featherEdit, featherExternalLink, featherMinus, featherMonitor, featherPlus, featherTrash, featherUpload, featherX } from '@ng-icons/feather-icons';
 
 import { routes } from './app.routes';
 
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideIcons({ featherPlus, featherMinus, featherX, featherTrash, featherEdit, featherCopy, featherExternalLink, featherDownload, featherMonitor }),
+    provideIcons({ featherPlus, featherMinus, featherX, featherTrash, featherEdit, featherCopy, featherExternalLink, featherDownload, featherMonitor, featherUpload }),
   ]
 };
